29156e0f06ec2ce23d6316a9051039a90602f721
RishikShr <48129871+RishikShr@users.noreply.github.com>
2021-05-27 03:13:52 +0530

    slice

--- my-app/src/components/Profile.js
+++ my-app/src/components/Profile.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import {useSelector,useDispatch} from'react-redux'
 //import { bindActionCreators } from 'redux'
-import {updateStatus} from '../actions'
+//import {updateStatus,fetchName} from '../actions'
+import {updateAge,updateName,updateStatus} from '../reducers/userReducer'
 
 const Profile = () => {
      
     const {name, age, status} = useSelector((state) => {return state })
     const dispatch = useDispatch()
-    const updateAge = (age)=>{    //!dispatching action
-        dispatch({
-             type:'UPDATE_AGE',
-             payload:age
-         })
+    const changeAge = (age)=>{    //!dispatching action
+        //dispatch({type:'UPDATE_AGE',payload:age})
+
+        dispatch(updateAge(age))  //? age is payload
    }
-   const updateName = (name)=>{    //!dispatching action
-    dispatch({
-         type:'UPDATE_NAME',
-         payload:name
-        })
+   const changeName = (name)=>{    //!dispatching action
+        
+        //const res = await fetch('https://jsonplaceholder.typicode.com/users')
+        //const result = await res.json()
+         
+        //dispatch({type:'UPDATE_NAME',payload: result[0].name})
+        dispatch(updateName(name))
     }
      //!dispatching action
     const updateStatuses = (status)=>{
@@ -30,8 +32,8 @@ const Profile = () => {
             <h2>i am {name}</h2>
             <h2>my age is {age}</h2>
             <h2>status is {status}</h2>
-            <button onClick={()=>updateAge(49)}>Update Age</button>
-            <button onClick={()=>updateName('Raju')}>Update Name</button>
+            <button onClick={()=>changeAge(49)}>Update Age</button>
+            <button onClick={()=>changeName("RAKESH")}>Update Name</button>
             <button onClick={()=>updateStatuses('Sleeping')}>Update Status</button>
 
         </div>
--- my-app/src/reducers/userReducer.js
+++ my-app/src/reducers/userReducer.js
@@ -1,5 +1,6 @@
-import {createReducer} from '@reduxjs/toolkit'
-import {updateStatus} from '../actions'
+import {createReducer,createSlice} from '@reduxjs/toolkit'
+//import {updateStatus} from '../actions'
+import { counterSlice } from '../features/counter/counterSlice'
 const initialState = {
     name:'Ramesh',
     age:20,
@@ -22,24 +23,43 @@ const initialState = {
 
 //}
 
+const userReducer =  createSlice({
+    name:'person',
+    initialState,
+    reducers:{
+        updateName:(state,action)=>{
+            state.name = action.payload
+        },
+        updateAge:(state,action)=>{
+            state.age  = action.payload
+        },
+        updateStatus:(state,action)=>{
+            state.status  = action.payload
+        }
 
+    }
+})
 
-export default createReducer(initialState,(builder)=>{
-   builder.addCase('UPDATE_AGE',(state,action)=>{
-       state.age= action.payload
-       })
-   builder.addCase('UPDATE_NAME',(state,action)=>{
-    state.name= action.payload
+export const {updateName,updateAge,updateStatus} = userReducer.actions
+export default userReducer.reducer
+
+// export default createReducer(initialState,(builder)=>{
+//    builder.addCase('UPDATE_AGE',(state,action)=>{
+//        state.age= action.payload
+//        })
+//    builder.addCase('UPDATE_NAME',(state,action)=>{
+//     state.name= action.payload
     
 
-})
-//builder.addCase('UPDATE_STATUS',(state,action)=>{
-//    state.status= action.payload
-//    })
+// })
+// //builder.addCase('UPDATE_STATUS',(state,action)=>{
+// //    state.status= action.payload
+// //    })
 
-builder.addCase(updateStatus,(state,action)=>{
-        state.status= action.payload
-        })
+// builder.addCase(updateStatus,(state,action)=>{
+//         state.status= action.payload
+//         })
     
 
-})
+// })
+
